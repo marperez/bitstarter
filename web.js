c469cdec44b76bad2bddc40b44413bff803b17a6
Mar Perez <marimar.perez@gmail.com>
2013-07-11 16:55:32 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = fs.readFileSync('./index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+    var buffer = fs.readFileSync('./index.html');
+    
+    response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
